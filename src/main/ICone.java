623e4c4f07126e2a7a2f47ac3a81387ee12d4534
Fong Lor <flor5@student.cvtc.edu>
2016-09-20 20:36:02 -0500

    Added cone interface + fixed ConeTest

---
+++ src/main/ICone.java
@@ -0,0 +1,12 @@
+package main;
+
+public interface ICone {
+
+	public void setHeight(float height);
+
+	public float getHeight();
+
+	public void setRadius(float radius);
+
+	public float getRadius();
+}
