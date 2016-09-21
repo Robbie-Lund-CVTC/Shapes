d89381ac387e3e2e259c70c4aa3aa804088af32b
rruf7 <rruf7@student.cvtc.edu>
2016-09-21 00:26:12 -0500

    Added Sphere Test

---
+++ src/test/SphereTest.java
@@ -0,0 +1,116 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import main.Sphere;
+import main.Constants;
+
+class SphereTest {
+
+	/** The diameter. */
+	private float diameter = 10;
+
+	/** The radius. */
+	private float radius = 5;
+
+	/** The sphere. */
+	private Sphere sphere = new Sphere(diameter, radius);
+
+	/**
+	 * Test Sphere constructor.
+	 */
+	@Test
+	public void testConeConstructor() {
+		float diameter = sphere.diameter;
+		float radius = sphere.radius;
+		assertEquals(this.diameter, diameter, Constants.DELTA);
+		assertEquals(this.radius, radius, Constants.DELTA);
+	}
+
+	/**
+	 * Test Diameter.
+	 */
+	@Test
+	public void testDiameter() {
+		float result = sphere.diameter;
+		assertEquals(diameter, result, Constants.DELTA);
+	}
+
+	/**
+	 * Test radius.
+	 */
+	@Test
+	public void testRadius() {
+		float result = sphere.radius;
+		assertEquals(radius, result, Constants.DELTA);
+	}
+
+	/**
+	 * Test set diameter.
+	 */
+	@Test
+	public void testSetDiameter() {
+		sphere.setDiameter(diameter);
+		assertEquals(diameter, sphere.diameter, Constants.DELTA);
+	}
+
+	/**
+	 * Test get diameter.
+	 */
+	@Test
+	public void testGetDiameter() {
+		float result = sphere.getDiameter();
+		assertEquals(diameter, result, Constants.DELTA);
+	}
+
+	/**
+	 * Test set radius.
+	 */
+	@Test
+	public void testSetRadius() {
+		sphere.setRadius(radius);
+		assertEquals(radius, sphere.radius, Constants.DELTA);
+	}
+
+	/**
+	 * Test get radius.
+	 */
+	@Test
+	public void testGetRadius() {
+		float result = sphere.getRadius();
+		assertEquals(radius, result, Constants.DELTA);
+	}
+
+	/**
+	 * Test sphere render.
+	 */
+	@Test
+	public void testConeRender() {
+		String result = sphere.Render();
+		String expected = "Diameter: " + sphere.diameter + " Radius: " + sphere.radius + " Volume: " + sphere.Volume()
+				+ " Surface Area: " + sphere.SurfaceArea();
+
+		assertEquals(expected, result);
+	}
+
+	/**
+	 * Test surface area.
+	 */
+	@Test
+	public void testSurfaceArea() {
+		float result = sphere.SurfaceArea();
+		assertEquals(314.16, result, Constants.DELTA);
+	}
+
+	/**
+	 * Test volume.
+	 */
+	@Test
+	public void testVolume() {
+		float result = sphere.Volume();
+		assertEquals(523.6, result, Constants.DELTA);
+	}
+
+}
